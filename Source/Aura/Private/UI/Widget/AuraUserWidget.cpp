289c2e64eb3eae420a5aeca7331e9998074c6e34
RealMasJaya <realmasjaya@gmail.com>
2025-02-02 18:27:24 +0700

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// copyright RealMasJaya
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+    WidgetControllerSet();
+}
