8f44e114111e5197f202a2e8a95e46e1f0f2e54e
RealMasJaya <realmasjaya@gmail.com>
2025-02-09 20:03:58 +0700

    Set By Caller Magnitude

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -114,4 +114,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("InputTag.4"),
 		FString("Input tag for 4 key")
 		);
+
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Damage"),
+		FString("Damage")
+		);
 }
