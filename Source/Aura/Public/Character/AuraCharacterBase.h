74046b8ef85d3bbf7ab3c28c66260cb70010e8b0
RealMasJaya <realmasjaya@gmail.com>
2025-02-17 11:47:37 +0700

    Demon Melee Attack

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -54,6 +54,9 @@ protected:
 
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	FName RightHandSocketName;
+	
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	FName TailSocketName;
 
 	bool bDead = false;
 	
