a845b00cfbbb27749952e185e7a75f0ac860d1c9
RealMasJaya <realmasjaya@gmail.com>
2025-02-24 11:45:10 +0700

    Spell Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// copyright RealMasJaya
+
+
+#include "UI/WidgetController/SpellMenuWidgetController.h"
+
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
