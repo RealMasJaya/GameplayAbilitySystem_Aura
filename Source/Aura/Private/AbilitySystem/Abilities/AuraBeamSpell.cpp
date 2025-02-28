d06f1387ac81339921d2e59c532704b9d6894ac0
RealMasJaya <realmasjaya@gmail.com>
2025-02-28 18:33:53 +0700

    Shock Loop Cues on Additional Targets

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -3,7 +3,9 @@
 
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
 {
@@ -26,4 +28,56 @@ void UAuraBeamSpell::StoreOwnerVariables()
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
 	}
 	
+}
+
+void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check(OwnerCharacter);
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			TArray<AActor*> ActorsToIgnore;
+			ActorsToIgnore.Add(OwnerCharacter);
+			FHitResult HitResult;
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			UKismetSystemLibrary::SphereTraceSingle(
+				OwnerCharacter,
+				SocketLocation,
+				BeamTargetLocation,
+				10.0f,
+				TraceTypeQuery1,
+				false,
+				ActorsToIgnore,
+				EDrawDebugTrace::None,
+				HitResult,
+				true);
+
+			if (HitResult.bBlockingHit)
+			{
+				MouseHitLocation = HitResult.ImpactPoint;
+				MouseHitActor = HitResult.GetActor();
+			}
+		}
+	}
+}
+
+void UAuraBeamSpell::StoreAdditionalTargets(TArray<AActor*>& OutAdditionalTargets)
+{
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
+	ActorsToIgnore.Add(MouseHitActor);
+
+	TArray<AActor*> OverlappingActors;
+	UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(
+		GetAvatarActorFromActorInfo(),
+		OverlappingActors,
+		ActorsToIgnore,
+		850.0f,
+		MouseHitActor->GetActorLocation());
+
+	//int32 NumAdditionalTargets = FMath::Min(GetAbilityLevel() - 1, MaxNumShockTargets);
+	int32 NumAdditionalTargets = 5;
+
+	UAuraAbilitySystemLibrary::GetClosestTargets(NumAdditionalTargets, OverlappingActors, OutAdditionalTargets, MouseHitActor->GetActorLocation());
 }
